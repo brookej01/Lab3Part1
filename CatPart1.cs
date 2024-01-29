8031f332a69ecbc98a39ec207501e83aaedaac67
Brooke <you@example.com>
2024-01-29 12:12:58 -0700

    Add project files.

---
+++ CatPart1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3Part1
+{
+    public class CatPart1 : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("Cats eat mice.");
+        }
+    }
+}
+        
+
---
+++ Part1Test.cs
@@ -0,0 +1,33 @@
+﻿
+using Lab3Part1;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter dog's name: ");
+        string dogName = Console.ReadLine();
+        DogPart1 dog = new DogPart1();
+        dog.Name = dogName;
+        dog.Colour = "Brown"; 
+        dog.Age = 3;
+        Console.WriteLine($"Dog's Name: {dog.Name}");
+        Console.WriteLine($"Dog's Colour: {dog.Colour}");
+        Console.WriteLine($"Dog's Age: {dog.Age}");
+        dog.Eat();
+
+        Console.Write("Enter cat's name: ");
+        string catName = Console.ReadLine();
+        CatPart1 cat = new CatPart1();
+        cat.Name = catName;
+        cat.Colour = "White"; 
+        cat.Age = 2;
+        Console.WriteLine($"Cat's Name: {cat.Name}");
+        Console.WriteLine($"Cat's Colour: {cat.Colour}");
+        Console.WriteLine($"Cat's Age: {cat.Age}");
+        cat.Eat();
+
+    }
+}
